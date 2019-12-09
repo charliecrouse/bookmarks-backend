2c4a148597acdc82f19a303e52a5b18b13ea2683
Charlie Crouse <charlietcrouse@icloud.com>
2019-12-08 23:58:57 -0500

    feature: make postgres config customizable

--- src/third-party/sequelize.ts
+++ src/third-party/sequelize.ts
@@ -3,16 +3,59 @@ import { Bookmark } from '../models/bookmark';
 import { Token } from '../models/token';
 import { User } from '../models/user';
 
+// Determine if executing in a docker container
 const DOCKER = (process.env.DOCKER || 'false').toLowerCase() === 'true';
 
-export const createDatabaseConnection = (): Sequelize => {
-  const host = DOCKER ? 'bookmarks-postgres' : '0.0.0.0';
+const getPostgresHost = (): string => {
+  const host = process.env.POSTGRES_HOST;
 
+  if (host) {
+    return host;
+  }
+
+  if (DOCKER) {
+    return 'bookmarks-postgres';
+  }
+
+  return '0.0.0.0';
+};
+
+const getPostgresUsername = (): string => {
+  const username = process.env.POSTGRES_USERNAME;
+
+  if (username) {
+    return username;
+  }
+
+  return 'postgres';
+};
+
+const getPostgresPassword = (): string => {
+  const password = process.env.POSTGRES_PASSWORD;
+
+  if (password) {
+    return password;
+  }
+
+  return '';
+};
+
+const getPostgresDatabase = (): string => {
+  const database = process.env.POSTGRES_DATABASE;
+
+  if (database) {
+    return database;
+  }
+
+  return 'postgres';
+};
+
+export const createDatabaseConnection = (): Sequelize => {
   const sequelize = new Sequelize({
-    host,
-    username: 'postgres',
-    password: '',
-    database: 'postgres',
+    host: getPostgresHost(),
+    username: getPostgresUsername(),
+    password: getPostgresPassword(),
+    database: getPostgresDatabase(),
     dialect: 'postgres',
     logging: false,
   });
